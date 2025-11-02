c560a4ac933b736a7785845b61980a5a9b743b14
root <root@ip-172-31-17-157.ec2.internal>
2025-11-02 00:55:00 +0000

    Actualización desde la instancia EC2 (backend + frontend integrados)

--- back-end/mi_api/urls.py
+++ back-end/mi_api/urls.py
@@ -1,8 +1,9 @@
+from django.urls import path, include , re_path
+from django.views.generic import TemplateView
 from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
    path('api/', include('core.urls')),
-
+   re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
+
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,8 @@
-// src/index.js
+// ✅ Todas las importaciones al inicio
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import axios from 'axios';
+import Cookies from 'js-cookie'; // 👈 Agrega aquí, junto con los demás imports
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,11 +13,20 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './estilos/personalizado.css';
 
-// Todas las llamadas a /api/... van a Django (ajusta si usas otro host/puerto)
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE || 'http://127.0.0.1:8000';
+// ✅ Configuración de axios
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://34.227.79.113:8000';
 axios.defaults.headers.common['Accept'] = 'application/json';
-// axios.defaults.withCredentials = true; // si usas sesión/CSRF
+axios.defaults.withCredentials = true;
 
+axios.interceptors.request.use((config) => {
+  const token = Cookies.get('csrftoken');
+  if (token) {
+    config.headers['X-CSRFToken'] = token;
+  }
+  return config;
+});
+
+// ✅ Render de React
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -25,3 +35,4 @@ root.render(
 );
 
 reportWebVitals();
+
